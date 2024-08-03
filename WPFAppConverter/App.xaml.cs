6d06e8eb03841c43a5e4345f1897ce5f37a6cff3
Alexander Kolyasa <84909252+PalmaLuv@users.noreply.github.com>
2024-08-03 20:45:54 +0300

    first push app

---
+++ WPFAppConverter/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPFAppConverter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ WPFAppConverter/MVVM/View/Window/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace WPFAppConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string TitleMainWindow { get; private set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            TitleMainWindow = "Converter";
+        }
+
+        public void LoadingMainWindow(object sender, RoutedEventArgs e)
+        {
+            //Loading Forms 
+        }
+
+        private void HeaderMouse(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
