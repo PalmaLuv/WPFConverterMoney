fbf9b02136da90f681530b1383eaf5710f31eae6
Alexander Kolyasa <84909252+PalmaLuv@users.noreply.github.com>
2024-08-05 16:01:13 +0300

    Update file

--- WPFAppConverter/Core/CoinAnalitic/CoinCap.cs
+++ WPFAppConverter/Core/CoinAnalitic/CoinCap.cs
@@ -2,50 +2,91 @@
 using System.Collections.Specialized;
 using System.Net.Http;
 using System.Windows;
+using Newtonsoft.Json;
+using System.Text.Json;
 using System.Windows.Controls;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace WPFAppConverter.Core.CoinAnalitic
 {
-    struct CoinStr
-    {
-        public string id;
-        public string rank;
-        public string symbol;
-        public string name;
-
-        public long supply;
-        public long maxSupply;
-        public long marketCapUsd;
-        public long volumeUsd24Hr;
-        public long priceUsd;
-        public long changePercent24Hr;
-        public long vwap24Hr;
-    }
-
     class CoinCap
     {
-        private string _apiURL = "api.coincap.io/v2/assets";
+        private static readonly HttpClient _client = new HttpClient 
+        { BaseAddress = new Uri("https://api.coincap.io") };
 
-        public CoinCap ()
-        { }
-
-        public async void GetAll()
+        public async Task GetElementMarkets(string id)
         {
-            var client = new HttpClient();
-            var request = new HttpRequestMessage(HttpMethod.Get, "api.coincap.io/v2/assets/bitcoin");
-            var response = await client.SendAsync(request);
-            response.EnsureSuccessStatusCode();
-            MessageBox.Show(await response.Content.ReadAsStringAsync());
-
-
-/*            using (var client = new HttpClient())
-            using (var reques = new HttpRequestMessage(HttpMethod.Get, "api.coincap.io/v2/assets/bitcoin"))
+            try
             {
-                var responce = await client.SendAsync(reques);
-                responce.EnsureSuccessStatusCode();
-                MessageBox.Show(await responce.Content.ReadAsStringAsync());
-            }*/
+
+            }
+            catch (HttpRequestException e)
+            {
+                 
+            }
+            catch (Exception e)
+            {
+
+            }
         }
-         
+
+        public async Task<Dictionary<string, CoinStruct>> GetAll()
+        {
+            var result = new Dictionary<string, CoinStruct>();
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, "/v2/assets");
+                var response = await _client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+
+                var jsonObject = JObject.Parse(await response.Content.ReadAsStringAsync());
+                var dataArray = jsonObject["data"] as JArray;
+                if (dataArray != null)
+                    foreach (var item in dataArray)
+                    {
+                        var id = item["id"]?.ToString();
+                        if (id != null)
+                            result.Add(id, item.ToObject<CoinStruct>());
+                    }
+            }
+            catch (HttpRequestException e)
+            {
+                HandleException(e);
+            }
+            catch (Exception e)
+            {
+                HandleException(e);
+            }
+            return result;
+        }
+
+        public async Task<CoinStruct> GetElement(string id)
+        {
+            var result = new CoinStruct();
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, $"/v2/assets/{id.ToLower()}");
+                var response = await _client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+
+                var jsonObject = JObject.Parse(await response.Content.ReadAsStringAsync());
+                var dataArray = jsonObject["data"] as JArray;
+                if (dataArray != null)
+                    result = dataArray[0].ToObject<CoinStruct>();
+            }
+            catch (HttpRequestException e)
+            {
+                HandleException(e);
+            }
+            catch (Exception e)
+            {
+                HandleException(e);
+            }
+            return result;
+        }
+
+        private void HandleException(Exception ex) =>
+            MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
     }
 }
---
+++ WPFAppConverter/Core/CoinAnalitic/CoinStruct.cs
@@ -0,0 +1,20 @@
+﻿namespace WPFAppConverter.Core.CoinAnalitic
+{
+    public struct CoinStruct
+    {
+        public string id;
+        public string rank;
+        public string symbol;
+        public string name;
+
+        public decimal supply;
+        public decimal? maxSupply;
+        public decimal marketCapUsd;
+        public decimal volumeUsd24Hr;
+        public decimal priceUsd;
+        public decimal changePercent24Hr;
+        public decimal vwap24Hr;
+    }
+
+    /* The structure is taken from the data API format. */
+}
